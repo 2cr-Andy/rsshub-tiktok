fe2e7e8402cedecbf3deb6aa283651d6e7b5520a
2cr-Andy <info@2cr.co.kr>
2025-07-11 17:32:57 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,13 @@
-module.exports = require('../lib/index')
+export default async function handler(req, res) {
+    try {
+        const { init } = await import('../lib/index.js');
+        const app = await init();
+        return app(req, res);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ 
+            error: 'Internal Server Error',
+            message: error.message 
+        });
+    }
+}
